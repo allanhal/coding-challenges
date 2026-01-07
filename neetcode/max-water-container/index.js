04894785e36f29452d7db1f29198399a9e327f48
Allan Araújo <allan.hal@gmail.com>
2026-01-06 22:45:49 -0300

    solved neetcode

--- neetcode/max-water-container/index.js
+++ neetcode/max-water-container/index.js
@@ -1,11 +1,26 @@
-let nums = [-1, 0, 1, 2, -1, -4];
+let input = [1, 7, 2, 5, 4, 7, 3, 6];
 
-function a() {}
+function maxArea(heights) {
+  let volume;
+  let indexVolumes;
+  for (let i = 0; i < heights.length; i++) {
+    for (let j = 0; j < heights.length; j++) {
+      const currentI = heights[i];
+      const currentJ = heights[j];
+      const indexDifference = Math.abs(i - j);
+      const minHeight = Math.min(currentI, currentJ);
+      const currentVolume = indexDifference * minHeight;
+      console.log("indexDifference", indexDifference, "minHeight", minHeight, "currentVolume", currentVolume);
+      if (volume === undefined || volume < currentVolume) {
+        volume = currentVolume;
+        indexVolumes = [i, j];
+      }
+    }
+  }
 
-console.log(a(nums));
+  return volume;
+}
+console.log("volume", maxArea(input));
 
-const output = [
-  [-1, -1, 2],
-  [-1, 0, 1],
-];
+const output = 36;
 // console.log({ output });
